6aa375d873b413ea6448439c4dd9f77d8d4a2d9e
Molly Mccann <mollymccann@bellevuecollege.edu>
2021-01-18 14:44:30 -0800

    Add project files.

---
+++ ch 3 dimystifying data types/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ch_3_dimystifying_data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of in: {0}", sizeof(int));
+            Console.ReadLine();
+
+        }
+    }
+}
